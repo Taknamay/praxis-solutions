1fec6086465c23af17b3b1ed20de51a6dd8bb698
Jason K. MacDuffie <taknamay@gmail.com>
2015-07-20 09:27:26 -0400

    Solve 9's and 0's with a brute force algorithm

--- 2015-06-19.scm
+++ 2015-06-19.scm
@@ -5,6 +5,8 @@
 ;; Given an integer n, find the smallest number consisting only of the
 ;; digits zero and nine that is divisible by n.
 
+;; I am using a brute-force algorithm to start with
+
 ;; Example: 23 --> 990909
 
 (import (scheme base)
@@ -17,6 +19,12 @@
 (define (only-9-0? n)
   (null? (delete #\0 (delete #\9 (number->digits n)))))
 
+(define (find-9-0-multiple n)
+  (let loop ((i 1))
+    (if (only-9-0? (* i n))
+        (* i n)
+        (loop (+ i 1)))))
 
-
+(display (find-9-0-multiple 23))
+(newline)
 
